571a5b73c562b4837ac4db1546f263fb700a29bc
colecody27 <colecody27@gmail.com>
2026-03-03 19:15:26 -0800

    Remove null default for venmo field

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,7 @@ class User(Document):
     auth0_id = StringField(required=True, unique=True)
     email = StringField(required=True, unique=True)
     phone = StringField()
-    venmo = StringField(null=True) 
+    venmo = StringField() 
     name = StringField()
     picture = StringField()
     provider = StringField()    
